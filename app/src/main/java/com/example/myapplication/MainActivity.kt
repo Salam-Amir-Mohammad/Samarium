fa89815dcba15d3c6b47e7e5c64da9bd74b62e01
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-07-04 18:05:01 +0330

    <Network> : i retrieve information

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,8 +15,8 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.myapplication.database.NetworkInfo
-import com.example.myapplication.database.NetworkInfoDatabaseHelper
+import android.telephony.*
+
 
 class MainActivity : AppCompatActivity(), LocationListener {
 
@@ -84,6 +84,81 @@ class MainActivity : AppCompatActivity(), LocationListener {
         if (hasRequiredPermissions()) {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_UPDATE_INTERVAL, 0f, this)
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, LOCATION_UPDATE_INTERVAL, 0f, this)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun displayNetworkInfo() {
+        val cellInfoList = telephonyManager.allCellInfo
+
+        for (cellInfo in cellInfoList) {
+            when (cellInfo) {
+                is CellInfoLte -> {
+                    updateLteCellInfo(cellInfo)
+                    break
+                }
+                is CellInfoWcdma -> {
+                    updateWcdmaCellInfo(cellInfo)
+                    break
+                }
+                is CellInfoGsm -> {
+                    updateGsmCellInfo(cellInfo)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun updateLteCellInfo(cellInfo: CellInfoLte) {
+        val cellIdentityLte = cellInfo.cellIdentity
+        val cellSignalStrengthLte = cellInfo.cellSignalStrength
+
+        tvPLMNID.text = "PLMN-ID: ${cellIdentityLte.mccString}${cellIdentityLte.mncString}"
+        tvTac.text = "TAC: ${cellIdentityLte.tac}"
+        tvCellId.text = "Cell ID: ${cellIdentityLte.ci}"
+        tvNetworkType.text = "4G (LTE)"
+
+        tvNetworkQuantity.text = "RSRQ: ${cellSignalStrengthLte.rsrq}, RSRP: ${cellSignalStrengthLte.rsrp}"
+        tvNetworkQuality.text = "Signal Strength: ${cellSignalStrengthLte.dbm} dBm"
+    }
+
+    private fun updateWcdmaCellInfo(cellInfo: CellInfoWcdma) {
+        val cellIdentityWcdma = cellInfo.cellIdentity
+        val cellSignalStrengthWcdma = cellInfo.cellSignalStrength
+
+        tvPLMNID.text = "PLMN-ID: ${cellIdentityWcdma.mccString}${cellIdentityWcdma.mncString}"
+        tvLac.text = "LAC: ${cellIdentityWcdma.lac}"
+        tvCellId.text = "Cell ID: ${cellIdentityWcdma.cid}"
+        tvNetworkType.text = "3G (WCDMA)"
+
+        tvNetworkQuantity.text = "RSCP: ${cellSignalStrengthWcdma.dbm}"
+        tvNetworkQuality.text = "Signal Strength: ${cellSignalStrengthWcdma.dbm} dBm"
+    }
+
+    private fun updateGsmCellInfo(cellInfo: CellInfoGsm) {
+        val cellIdentityGsm = cellInfo.cellIdentity
+        val cellSignalStrengthGsm = cellInfo.cellSignalStrength
+
+        tvPLMNID.text = "PLMN-ID: ${cellIdentityGsm.mccString}${cellIdentityGsm.mncString}"
+        tvLac.text = "LAC: ${cellIdentityGsm.lac}"
+        tvCellId.text = "Cell ID: ${cellIdentityGsm.cid}"
+        tvNetworkType.text = "2G (GSM)"
+
+        tvNetworkQuantity.text = "RSSI: ${cellSignalStrengthGsm.dbm}"
+        tvNetworkQuality.text = "Signal Strength: ${cellSignalStrengthGsm.dbm} dBm"
+    }
+
+    override fun onLocationChanged(location: Location) {
+        tvLatitude.text = "Latitude: ${location.latitude}"
+        tvLongitude.text = "Longitude: ${location.longitude}"
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            startLocationUpdates()
+            displayNetworkInfo()
         }
     }
 }
